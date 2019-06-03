b4171c54d27a934592cde0db8d11711cb8d16d42
Danielandro <mtme13@hotmail.com>
2019-06-03 15:55:58 +0100

    add deduct method to card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,13 +8,19 @@ describe Oystercard do
   end
 
   describe '#top_up' do
-    it { is_expected.to respond_to(:top_up).with(1).argument }
-
+    
     it 'increases balance by 10' do
       expect { subject.top_up(10) }.to change { subject.balance }.by(10)
     end
+
     it "prevents balance exceeding £#{Oystercard::BALANCE_LIMIT}" do
       expect { subject.top_up(100) }.to raise_error("Balance cannot be over £#{Oystercard::BALANCE_LIMIT}")
+    end
+  end
+
+  describe '#deduct' do
+    it 'reduces balance by 10' do
+      expect { subject.deduct(10) }.to change { subject.balance }.by(-10)
     end
   end
 
