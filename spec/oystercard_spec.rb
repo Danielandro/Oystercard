9404ed599c57a979c56e38566b33c31b3bb1a519
Danielandro <mtme13@hotmail.com>
2019-06-03 15:27:59 +0100

    .

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,13 +1,18 @@
 require 'oystercard'
 
 describe Oystercard do
-  it 'knows Oystercard class exists' do
-    expect(described_class).to eq(Oystercard)
-  end
-
   describe 'Initialization' do
     it 'has a balance of 0' do
       expect(subject.balance).to eq(0)
     end
   end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'increases balance by 10' do                
+      expect(subject.top_up(10)).to eq(10)
+    end
+  end
+
 end
