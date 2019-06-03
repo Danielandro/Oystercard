2997d688108afa690bf68cd1b113b353545ee783
Danielandro <mtme13@hotmail.com>
2019-06-03 17:17:30 +0100

    Add test to charge for journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -30,6 +30,7 @@ class Oystercard
   end
 
   def touch_out
+    @balance -= MINIMUM_FARE
     @in_journey = false
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -49,6 +49,13 @@ describe Oystercard do
         subject.touch_out
         expect(subject).not_to be_in_journey
       end
+
+      it 'deducts correct amount' do
+        subject.top_up(@amount)
+        subject.touch_in
+        
+        expect { subject.touch_out }.to change { subject.balance }.by(-Oystercard::MINIMUM_FARE)
+      end
     end
   end
 
