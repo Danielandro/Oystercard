1ebf8c4695fdad4c68cd030f1f03cd0cb8777ef4
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-06-03 14:39:08 +0100

    initializes with balance of 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,13 @@
 require 'oystercard'
 
 describe Oystercard do
+  it 'knows Oystercard class exists' do
+    expect(described_class).to eq(Oystercard)
+  end
+
+  describe 'Initialization' do
+    it 'has a balance of 0' do
+      expect(subject.balance).to eq(0)
+    end
+  end
 end
